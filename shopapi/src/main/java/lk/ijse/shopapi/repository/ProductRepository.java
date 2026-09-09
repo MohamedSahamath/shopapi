8f9c3b43a0eec0e85ddb16373c410b89af1a9c8d
MohamedSahamath <iamsahamath@gmail.com>
2026-09-09 18:38:46 +0530

    add low stock query

--- shopapi/src/main/java/lk/ijse/shopapi/repository/ProductRepository.java
+++ shopapi/src/main/java/lk/ijse/shopapi/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package lk.ijse.shopapi.repository;
 
 import lk.ijse.shopapi.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,4 +11,12 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategoryId(Long categoryId);
     List<Product> findByNameContainingIgnoreCaseAndActiveTrue(String name);
     boolean existsBySku(String sku);
+
+    @Query(value = """
+            SELECT p.* FROM products p
+            WHERE p.active = true
+              AND p.stock_quantity <= p.reorder_level
+            ORDER BY p.stock_quantity ASC
+            """, nativeQuery = true)
+    List<Product> findLowStockProducts();
 }
